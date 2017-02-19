3caf610166fd0abd67e24bc15815e9cfe29e7912
jasecoop <jase@jasecoop.com>
2017-02-19 22:12:06 +0000

    vimeo

--- _app/js/lib/lightYoutubeEmbeds.js
+++ _app/js/lib/lightYoutubeEmbeds.js
@@ -1,33 +1,41 @@
 module.exports = function () {
-    /* Light YouTube Embeds by @labnol */
-    /* Web: http://labnol.org/?p=27941 */
+/* Adapted from
+   Light YouTube Embeds by @labnol
+   http://labnol.org/?p=27941 */
 
-    document.addEventListener("DOMContentLoaded",
-        function() {
-            var div, n,
-                v = document.getElementsByClassName("youtube-player");
-            for (n = 0; n < v.length; n++) {
-                div = document.createElement("div");
-                div.setAttribute("data-id", v[n].dataset.id);
-                div.innerHTML = labnolThumb(v[n].dataset.id);
-                div.onclick = labnolIframe;
-                v[n].appendChild(div);
-            }
-        });
+function lightEmbedInit() {
 
-    function labnolThumb(id) {
-        var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
-            play = '<div class="play"></div>';
-        return thumb.replace("ID", id) + play;
-    }
+  var div, n,
+      v = document.getElementsByClassName('light-video-embed');
 
-    function labnolIframe() {
-        var iframe = document.createElement("iframe");
-        var embed = "https://www.youtube.com/embed/ID?autoplay=1?rel=0";
-        iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
-        iframe.setAttribute("frameborder", "0");
-        iframe.setAttribute("allowfullscreen", "1");
-        this.parentNode.replaceChild(iframe, this);
-    }
+  for (n = 0; n < v.length; n++) {
+
+    v[n].onclick = function() {
+
+      var id = this.dataset.id;
+      var img_src = new URL(this.children[0].attributes[0].value);
+      var hostname = img_src.hostname;
+
+      var iframe = document.createElement('iframe');
+
+      if (hostname == 'i.ytimg.com') {
+        var embed = 'https://www.youtube.com/embed/ID?autoplay=1';
+      }
+      else if (hostname == 'i.vimeocdn.com') {
+        var embed = '//player.vimeo.com/video/ID?autoplay=1';
+      }
+
+      iframe.setAttribute('src', embed.replace('ID', id));
+      iframe.setAttribute('frameborder', '0');
+      iframe.setAttribute('allowfullscreen', '1');
+      this.parentNode.replaceChild(iframe, this);
+    };
+  }
+}
+
+      document.addEventListener('DOMContentLoaded', function() {
+        lightEmbedInit();
+      });
+
 
 }

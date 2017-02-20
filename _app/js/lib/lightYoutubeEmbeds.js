5b99b9470fbc0fb46895618e9625757cf0384ae3
jasecoop <jase@jasecoop.com>
2017-02-20 02:17:57 +0000

    fix

--- _app/js/lib/lightYoutubeEmbeds.js
+++ _app/js/lib/lightYoutubeEmbeds.js
@@ -13,17 +13,17 @@ function lightEmbedInit() {
     v[n].onclick = function() {
 
       var id = this.dataset.id;
-      var img_src = new URL(this.children[0].attributes[0].value);
-      var hostname = img_src.hostname;
+      // var img_src = new URL(this.children[0].attributes[0].value);
+      // var hostname = img_src.hostname;
 
       var iframe = document.createElement('iframe');
 
-      if (hostname == 'i.ytimg.com') {
-        var embed = 'https://www.youtube.com/embed/ID?autoplay=1';
-      }
-      else if (hostname == 'i.vimeocdn.com') {
+      // if (hostname == 'i.ytimg.com') {
+        // var embed = 'https://www.youtube.com/embed/ID?autoplay=1';
+      // }
+      // else if (hostname == 'i.vimeocdn.com') {
         var embed = '//player.vimeo.com/video/ID?autoplay=1&loop=1&title=0';
-      }
+      // }
 
       iframe.setAttribute('src', embed.replace('ID', id));
       iframe.setAttribute('frameborder', '0');
